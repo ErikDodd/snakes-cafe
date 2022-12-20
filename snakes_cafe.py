dc6d25534b773f594f70c09eb37cfe78890def5c
ERIK M. DODD <emdodd27@gmail.com>
2022-12-19 17:31:53 -0800

    added quit functionality. Should be all done now

--- snakes_cafe.py
+++ snakes_cafe.py
@@ -38,22 +38,26 @@ LaCroix
 ***********************************
     """)
     food_order = input("> ")
+    if food_order == "quit":
+        quit()
     print(f"** 1 order of {food_order} has been added to your meal **")
 
     ask_again = True
     while ask_again:
         print("Would you like to order other items?")
         next_order = input("> ")
+        if next_order == "quit":
+            quit()
         if next_order == "yes":
             print("What else would you like to order?")
             more_food = input("> ")
+            if more_food == "quit":
+                quit()
             print(f"** 1 order of {more_food} has been added to your meal **")
         else:
             break
     # print(f"** Your order is {food_order} and {more_food} **")
 
+
 menu()
-
-
-
 
