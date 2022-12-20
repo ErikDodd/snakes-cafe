bdb14f3bf1b853800ef956c449f06fc13ba56236
ERIK M. DODD <emdodd27@gmail.com>
2022-12-19 16:09:35 -0800

    Menu and welcome for lab 1 completed

---
+++ snakes_cafe.py
@@ -0,0 +1,40 @@
+def menu():
+    print("""
+**************************************
+**    Welcome to the Snakes Cafe!   **
+**    Please see our menu below.    **
+**                                  **
+** To quit at any time, type "quit" **
+**************************************
+""")
+    print("""
+Appetizers
+----------
+Egg Rolls
+Dumplings
+Pot Stickers
+
+Entrees
+-------
+Pho'
+Vermicelli
+Pad Thai
+Banh Mi
+Green Curry
+
+
+
+Desserts
+--------
+Rootbeer Float
+Ice Cream Sundae
+Brownies
+
+Drinks
+------
+Beer
+Coca-Cola
+LaCroix
+    """)
+
+menu()
