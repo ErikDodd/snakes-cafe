d4253c8b2740c6efe24e38536b011dc6dfeb14b1
ERIK M. DODD <emdodd27@gmail.com>
2022-12-26 17:23:10 -0800

    Updated code to increment orders. Also, refactored to not be DRY

--- snakes_cafe.py
+++ snakes_cafe.py
@@ -32,32 +32,48 @@ Drinks
 Beer
 Coca-Cola
 LaCroix
+    """)
 
+def food_order():
+    print(""" 
 ***********************************
 ** What would you like to order? **
 ***********************************
     """)
-    food_order = input("> ")
-    if food_order == "quit":
-        quit()
-    print(f"** 1 order of {food_order} has been added to your meal **")
-
+    food_order_dict = {}
     ask_again = True
     while ask_again:
-        print("Would you like to order other items?")
-        next_order = input("> ")
-        if next_order == "quit":
-            quit()
-        if next_order == "yes":
-            print("What else would you like to order?")
-            more_food = input("> ")
-            if more_food == "quit":
-                quit()
-            print(f"** 1 order of {more_food} has been added to your meal **")
+        food_order = input("> ")
+        if food_order in food_order_dict:
+            food_order_dict[food_order] += 1
         else:
+            food_order_dict[food_order] = 1
+        for key in food_order_dict:
+            if food_order != "quit":
+                print(f"** {food_order_dict[key]} orders of {key} has been added to your meal **")
+        if food_order == "quit":
+            print("Thank you for vising Snakes Cafe!")
+            quit()
             break
+    #print(f"** 1 order of {food_order} has been added to your meal **")
+
+    #ask_again = True
+    #while ask_again:
+        #print("Would you like to order other items?")
+        #next_order = input("> ")
+        #if next_order == "quit":
+            #quit()
+        #if next_order == "yes":
+            #print("What else would you like to order?")
+           # more_food = input("> ")
+            #if more_food == "quit":
+                #quit()
+            #print(f"** 1 order of {more_food} has been added to your meal **")
+        #else:
+            #break
     # print(f"** Your order is {food_order} and {more_food} **")
 
 
 menu()
+food_order()
 
